dc033ff9dd8326bb40bb1cbab48ef92f32532eeb
palhamel <pal.hamel@gmail.com>
2020-04-12 16:47:45 +0200

    First commit

---
+++ src/components/Board.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Square } from 'components/Square'
+
+export const Board = () => {
+  // TODO - use the 'squares' state from the game reducer:
+  // const squares = []
+  // This takes the data from store.game.squares, out of the STORE
+  const squares = useSelector((store) => (store.game.squares))
+
+  return (
+    <div className="board">
+      {squares.map((value, index) => (
+        <Square key={index} value={value} index={index} />
+      ))}
+    </div>
+  )
+}
---
+++ src/components/Square.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { Player } from 'components/Player'
+
+export const Square = ({ value, index }) => {
+  const handleClick = () => {
+    // TODO send the captureSquare action
+
+  }
+
+  return (
+    <button
+      className={value === null ? 'square clickable' : 'square'}
+      type="button"
+      onClick={handleClick}>
+      <Player value={value} />
+    </button>
+  )
+}
